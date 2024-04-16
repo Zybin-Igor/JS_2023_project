d9101a23f8e7f094a635ca770f4dc43e74d0b965
Ihor Zybin <zibi_man@ukr.net>
2024-04-16 18:13:18 +0200

    first commit web-site part1

---
+++ js/script.js
@@ -0,0 +1,13 @@
+"user strict"
+// let out = document.getElementById("out");
+// let result = "";
+// const length = 10;
+// for (let i = 1; i < length; i=i+1){
+// 	for (let j = 10; j > i; j--){
+// 		result += "*";
+// 	}
+// 	result += "\n";
+// }
+// console.log(result);
+// out.innerHTML = result;
+alert("Hello!");
