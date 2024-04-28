e5d3c39b7a297598dd5f2b7b039e49de3acda2df
Ihor Zybin <zibi_man@ukr.net>
2024-04-28 16:40:54 +0200

    Создание кнопок и интерактив

--- js/script.js
+++ js/script.js
@@ -10,4 +10,14 @@
 // }
 // console.log(result);
 // out.innerHTML = result;
-alert("Hello!");
+// let variable1 = 3;
+// console.log(variable1);
+// let calculation = 2 + 2;
+// console.log(calculation);
+// console.log(calculation + 2);
+// let message = "hello";
+// console.log(message);
+// variable1 = 5; //переназначение переменной
+/*This is a comment*/
+const cartQuantity = 0;
+// остановился на времени 2:40:32
